a643b66d870858a7d099a2676704fe53efa65633
bsmaciel-ifb <bruno.maciel@ifb.edu.br>
2026-05-16 15:30:42 -0300

    Aula-12, parte-2

---
+++ aula12/ex012.js
@@ -0,0 +1,10 @@
+var clock = new Date();
+var hora = clock.getHours();
+console.log(`Agora são exatamente ${hora} horas.`);
+if (hora < 12) {
+  console.log("Bom dia!");
+} else if (hora <= 18) {
+  console.log("Boa tarde!");
+} else {
+  console.log("Boa noite!");
+}

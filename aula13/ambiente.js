95b6beb73c54fe78b6d2c1d4c920e73f7a77d713
bsmaciel-ifb <bruno.maciel@ifb.edu.br>
2026-06-09 09:16:02 -0300

    aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+/* 
+console.log('Passo 1');
+console.log('Passo 2');
+console.log('Passo 3');
+console.log('Passo 4');
+console.log('Passo 5');
+console.log('Passo 6'); 
+*/
+/* var contador = 1;
+while (contador <= 6) {
+	console.log(`Passo ${contador}`);
+	contador++; // c = c + 1
+} */
+var contador = 1;
+do {
+	console.log(`Passo ${contador}`);
+	contador++; // c = c + 1
+} while (contador <= 2);

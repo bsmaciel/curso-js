86e5eeecb85ec5fed9d1374635f48abde955680c
bsmaciel-ifb <bruno.maciel@ifb.edu.br>
2026-06-23 10:53:19 -0300

    Aula 14 ex16 e início do ex17

---
+++ aula14/aula14ex/ex17/script.js
@@ -0,0 +1,13 @@
+function tabuada() {
+	let num = document.getElementById('txtn');
+	let tab = document.getElementById('seltab');
+	if (num.value.length == 0) {
+		window.alert('Por favor, digite um número!');
+	} else {
+		let n = Number(num.value);
+		let c = 1;
+		while (c <= 10) {
+			let item = document.createElement('option');
+		}
+	}
+}
